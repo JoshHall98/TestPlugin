227e3494ae98a84a4ee805a4e2f7ae32f49ad878
Hall <JHALL212@caledonian.ac.uk>
2022-03-09 17:02:57 +0000

    Add dB Control

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -53,7 +53,9 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
-private:
+private: 
+
+    float dB; 
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TestPluginAudioProcessor)
 };
